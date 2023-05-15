16a884df9884379438a96e92cc44e37278d95812
lala11112 <lala11112@naver.com>
2023-05-15 20:41:26 +0900

    Add files via upload
    프로그래밍 기초 때 배운 C# 클래스 예제

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp3
+{
+    class Car
+    {
+        private double parkingTime = 0;
+        DateTime inTime;
+        DateTime outTime;
+        public Car(string carName)
+        {
+
+        }
+        public void  SetInTime()
+        {
+            inTime = DateTime.Now;
+
+        }
+        public void Park()
+        {
+            Random random = new Random();
+            int parkTi = random.Next(300);
+            Console.WriteLine(parkTi);
+            Thread.Sleep(parkTi);
+        }
+        public void SetOutTime()
+        {
+            outTime = DateTime.Now;
+            parkingTime = outTime.Millisecond - inTime.Millisecond;
+            Console.WriteLine(parkingTime);
+        }
+        public void FeeSettle()
+        {
+            if(parkingTime < 100)
+            {
+                Console.WriteLine("주차비는 1000원입니다.");
+            }
+            else if (parkingTime > 100 && parkingTime < 200)
+            {
+                Console.WriteLine("주차비는 2000원입니다.");
+            }
+            else if (parkingTime > 200)
+            {
+                Console.WriteLine("주차비는 3000원입니다.");
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("16하6399");
+            car.SetInTime();
+            car.Park();
+            car.SetOutTime();
+            car.FeeSettle();
+
+            Console.ReadKey();
+        }
+    }
+}
