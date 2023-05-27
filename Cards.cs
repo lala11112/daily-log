e15618e9baa9235975b02577cbe0ae8886720acd
lala11112 <lala11112@naver.com>
2023-05-27 20:00:15 +0900

    Add files via upload
    포커게임 만들기 1

---
+++ Cards.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Cards
+    {
+        public static Card[] card = new Card[52]; // Card 52장 생성
+
+        public Cards() { // Cards의 생성자 함수
+            int checkNum = 0;
+            int checkSuit = 0;
+            for(int i = 0; i < card.Length; i++)
+            {
+                if(i % 13 == 0 && i != 0)
+                {
+                    checkNum = 0;
+                    checkSuit++;
+                }
+                checkNum++;
+                card[i].suit = (Suit)checkSuit;
+                card[i].number = checkNum;
+            }
+        }
+
+        public static void CheckCard()
+        {
+        }
+        public static void Distribute()
+        {
+
+        }
+        public static void ShowCards() // 카드를 보여주는 함수
+        {
+            for(int i  = 0; i < card.Length; i++)
+            {
+                Console.WriteLine(card[i].suit + "의" + card[i].number);
+            }
+        }
+        public static void Shuffle()
+        {
+            Random random = new Random();
+            for(int i=0; i < card.Length; i++)
+            {
+                int randomNum = random.Next(card.Length);
+                
+                Card temp = card[i];
+                card[i] = card[randomNum];
+                card[randomNum] = temp;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,136 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ItemEatGame
+﻿namespace Poker
 {
-    class Point
-    {
-        public int x;
-        public int y;
-        public Point(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-    }
     class Program
     {
-        enum Mark { empty, player, item }
-        static int screenX = 50, screenY = 25;
-        static Mark[,] screen = new Mark[screenX, screenY];
-        static List<Point> itemList = new List<Point>();
-        static int px = 25, py = 12;
-        static bool isGameOn = true;
         static void Main(string[] args)
         {
-            ClearScreen(); // screen을 empty로 채움
-            MakeItem(); // 아이템 위치 생성 후 리스트 및 스크린에 보관
-            DrawScreen(); // 스크린 그리기
-            while (isGameOn == true)
-            {
-                MovePlayer();
-                CheckState();
-            }
-            ExitGame();
-        }
-        static void ClearScreen()
-        {
-            for (int i = 0; i < screen.GetLength(0); i++)
-            {
-                for (int j = 0; j < screen.GetLength(1); j++)
-                {
-                    screen[i, j] = Mark.empty;
-                }
-            }
-        }
-        static void MakeItem()
-        {
-            Random random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                int x = random.Next(1, screenX - 1);
-                int y = random.Next(1, screenY - 1);
-                Point point = new Point(x, y);
-                itemList.Add(point);
-                screen[x, y] = Mark.item;
-            }
-        }
-        static void DrawScreen()
-        {
-            for (int i = 0; i < screen.GetLength(0); i++)
-            {
-                for (int j = 0; j < screen.GetLength(1); j++)
-                {
-                    if (screen[i, j] == Mark.item)
-                    {
-                        Console.SetCursorPosition(i, j); // 그 좌표에 콘솔 커서 놓기
-                        Console.Write("$"); // 아이템 좌표에 $ 출력하기
-                    }
-
-                }
-            }
-            ShowCursorPosition();
-        }
-        private static void ShowCursorPosition()
-        {
-            Console.SetCursorPosition(15, 30);
-            Console.Write("============ X좌표: " + px + " Y좌표: " + py + " 남은 아이템 갯수: " + itemList.Count + " ============"); // 플레이어의 X좌표, Y좌표, 남은 아이템 갯수를 출력
-
-        }
-        static void MovePlayer()
-        {
-            ConsoleKeyInfo cki = Console.ReadKey();
-            Console.SetCursorPosition(px, py);
-            Console.WriteLine(" ");
-
-            switch (cki.Key)
-            {
-                case ConsoleKey.LeftArrow:
-                    px--;
-                    break;
-                case ConsoleKey.RightArrow:
-                    px++;
-                    break;
-                case ConsoleKey.UpArrow:
-                    py--;
-                    break;
-                case ConsoleKey.DownArrow:
-                    py++;
-                    break;
-            }
-            if (px >= screenX) px = 1;
-            if (py >= screenY) py = 1;
-            if (px <= 0) px = screenX - 1;
-            if (py <= 0) py = screenY - 1;
-            Console.SetCursorPosition(px, py);
-            Console.Write("P");
-            ShowCursorPosition();
-        }
-        static void CheckState()
-        {
-            if (screen[px, py] == Mark.item)
-            {
-                for (int i = itemList.Count - 1; i >= 0; i--)
-                {
-                    if (itemList[i].x == px && itemList[i].y == py)
-                    {
-                        itemList.Remove(itemList[i]);
-                        if (itemList.Count == 0)
-                        {
-                            isGameOn = false;
-                        }
-                    }
-                }
-
-                screen[px, py] = Mark.empty;
-            }
-        }
-        static void ExitGame()
-        {
-            Console.Write("게임 끝");
+            Cards cards = new Cards();
+            Cards.ShowCards();
+            Console.WriteLine("---------- 섞어~ 섞어~ ----------");
+            Cards.Shuffle();
+            Cards.ShowCards();
         }
     }
 }
